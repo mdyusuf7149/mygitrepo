8b000cde45ae53f50aec083bbf0eedf46f7567b2
mdyusuf7149@gmail.com <sr.yusuf7149@outlook.com>
2024-09-02 06:51:53 +0530

    complete task

--- javaregfile.java
+++ javaregfile.java
@@ -1,2 +1,10 @@
 This is Java Registration page
+<<<<<<< Updated upstream
 This is my managaer work -- Completed
+=======
+1st day java lines of code
+2nd day java lines of code
+3rd day java lines of code
+4th day java lines of code
+>>>>>>> Stashed changes
+5th day java lines of code
